4f308249369e8b45855a36420378fbfb2bf30c46
schenzio <56231279+schenzio@users.noreply.github.com>
2022-01-16 15:40:52 +0100

    replace apos with correct entity

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home( {allPostsData} ) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm your friendly neighrborhood full stack developer</p>
+        <p>Hi, I&apos;m your friendly neighrborhood full stack developer</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '} {/* adds an empty space, which is used to divide text over multiple lines*/}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
